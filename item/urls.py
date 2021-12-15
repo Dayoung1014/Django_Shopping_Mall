498bbc88812266588a835830b7a57e3fefc953d6
Dayoung1014 <wsqqa1014@qmail.com>
2021-12-15 20:50:13 +0900

    검색, 카테고리 위젯 기능 구현

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ItemList.as_view()), #서버 IP/item
     path('<int:pk>/', views.ItemDetail.as_view()),
+    path('search/<str:q>/', views.ItemSearch.as_view()),
+    path('category/<str:slug>', views.category_page),
     #path('register_item/', views.ItemRegister.as_view()),
 ]
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
-from .models import Item
+from .models import Item, Category
+from django.db.models import Q
 
 # Create your views here.
 class ItemList(ListView):
@@ -9,6 +9,51 @@ class ItemList(ListView):
     ordering = 'pk'
     paginate_by = 8
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ItemList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_item_count'] = Item.objects.filter(category=None).count()
+        return context
+
+class ItemSearch(ItemList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        item_list = Item.objects.filter(
+            Q(name__contains=q) | Q(company__name__contains=q)
+        ).distinct()
+        return item_list
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemSearch, self).get_context_data()
+        q  = self.kwargs['q']
+        context['search_info'] = f'{q}({self.get_queryset().count()})'
+        return context
+
 class ItemDetail(DetailView):
     model = Item
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ItemDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_item_count'] = Item.objects.filter(category=None).count()
+        return context
+
+def category_page(request, slug):
+    if slug == 'no_category' :
+        category = '미분류'
+        item_list = Item.objects.filter(category=None) #일치하는
+
+    else:
+        category = Category.objects.get(slug=slug)
+        item_list = Item.objects.filter(category=category)
+
+    return render(request, 'item/item_list.html',
+            {
+                'item_list' : item_list,
+                'categories' : Category.objects.all(),
+                'no_category_post_count' : Item.objects.filter(category=None).count(),
+                'category' : category
+            }
+        )
